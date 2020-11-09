146267160f084653d8d86ddf704b2490fd24f79b
Michalis Chaviaras <michav1510@gmail.com>
2020-11-09 19:10:21 +0200

    simple test added

--- CarRental.UnitTests/UnitTest1.cs
+++ CarRental.UnitTests/UnitTest1.cs
@@ -1,14 +1,18 @@
 using System;
 using Xunit;
+using CarRental;
 
 namespace CarRental.UnitTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsPrime_InputIs1_ReturnFalse()
         {
+            var fortestclass = new forTestClass();
+            bool result = fortestclass.IsPrime(1);
 
+            Assert.False(result, "1 should not be prime");
         }
     }
 }
---
+++ CarRental/forTestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental
+{
+    public class forTestClass
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+
+    }
+}
