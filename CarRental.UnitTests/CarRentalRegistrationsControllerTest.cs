b765ca497713f3cfe0880361812adf24356f55d6
Michalis Chaviaras <michav1510@gmail.com>
2020-11-11 12:36:20 +0200

    The first test runned. I have now to clean the repos and contract folders and develop more tests

--- CarRental.UnitTests/CarRentalRegistrationsControllerTest.cs
+++ CarRental.UnitTests/CarRentalRegistrationsControllerTest.cs
@@ -1,26 +1,66 @@
 using Xunit;
 using CarRental.Controllers;
 using CarRental.Models;
+using Moq;
+using CarRental.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CarRental.UnitTests
 {
     public class CarRentalRegistrationsControllerTest
     {
-        CarRentalRegistrationsController _controller;
-        ICarRentalRegistrationsService _service;
-        public CarRentalRegistrationsControllerTest()
-        {
-            _service = new CarRentalRegistrationsFake();
-            _controller = new CarRentalRegistrationsController();
-        }
-
         [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        public async void IsPrime_InputIs1_ReturnFalse()
         {
-            var fortestclass = new forTestClass();
-            bool result = fortestclass.IsPrime(1);
+            //create In Memory Database
+            var options = new DbContextOptionsBuilder<CarRentalContext>()
+            .UseInMemoryDatabase(databaseName: "TodoList")
+            .Options;
 
-            Assert.False(result, "1 should not be prime");
+
+            //// Create mocked Context by seeding Data as per Schema///
+
+            using (var context = new CarRentalContext(options))
+            {
+                context.CarRentalRegistrations.Add(new CarRentalRegistration
+                {
+                    Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                    CustomerSocSecNum = 15100,
+                    RegistrNum = 150899001,
+                    DateOfDeli = DateTime.Now,
+                    KmAtDelivery = 150000,
+                    VehicleCat = CarRentalRegistration.VehicleCategory.SmallCar
+                });
+
+                context.CarRentalRegistrations.Add(new CarRentalRegistration
+                {
+                    Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
+                    CustomerSocSecNum = 15100,
+                    RegistrNum = 150884304,
+                    DateOfDeli = DateTime.Now,
+                    KmAtDelivery = 90000,
+                    VehicleCat = CarRentalRegistration.VehicleCategory.SmallCar
+                });
+                context.SaveChanges();
+            }
+
+            using (var context = new CarRentalContext(options))
+            {
+                Guid id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+                CarRentalRegistrationsController controller = new CarRentalRegistrationsController(context);
+                var result = await controller.GetCarRentalRegistration(id);
+
+                //Console.WriteLine("Typeof(actualresult)= "+actualresult);
+                Assert.Equal(result.Value.Id, id);
+
+
+                //var actualResult = result.Value;
+                //Assert.Equal("12345", ((EmployeeDb)actualResult).Id);
+            }
+
         }
     }
 }
